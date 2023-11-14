e78dabd9b62fd727dd479d37daad0eb7bb00f3d8
VarunShukla07 <varun.shuklaoffical@gmail.com>
2023-11-14 21:21:09 +0530

    Add Loading Effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+        <img className="h-96" src="spinner.svg" alt="Loading..." />
+    </div>
+  )
+}
