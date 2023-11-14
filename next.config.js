d957279f60e78635babe230a23c30699e031d609
VarunShukla07 <varun.shuklaoffical@gmail.com>
2023-11-14 23:26:45 +0530

    Add Card component and fix api bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      domains: ["image.tmdb.org"],
+    },
+  };
 
 module.exports = nextConfig
