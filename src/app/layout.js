4bc94f05d7f2d1fad43c9ec743ec7f71bc506bf8
VarunShukla07 <varun.shuklaoffical@gmail.com>
2023-11-14 12:51:03 +0530

    Add Dark Mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import './globals.css'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,13 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        
-        {/* Header */}
-        <Header/>
-        {/* Navbar */}
-        {/* Foot */}
-        {children}
-        
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
+          {/* Foot */}
+          {children}
+        </Providers>
+
+
       </body>
     </html>
   )
