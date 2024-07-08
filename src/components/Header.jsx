d772f5b020fb322906dcbae895902326c0fc82af
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-07-08 21:54:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,8 +16,8 @@ export default function Header() {
           <DarkModeSwitch/>
             <Link href="/">
                 <h2 className="text-2xl"><span className="font-bold bg-amber-500
-                py-1 px-2 rounded-lg mr-1">IMDb</span>
-                <span className="text-xl hidden sm:inline">Clone</span></h2>
+                py-1 px-2 rounded-lg mr-1">CiNe</span>
+                <span className="text-xl hidden sm:inline">Verse</span></h2>
             </Link>
         </div>
     </div>
