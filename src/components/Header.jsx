57e07322875bc9034c676727d504e40a98c26a09
Varun Shukla <94779584+VarunShukla07@users.noreply.github.com>
2024-08-06 16:51:47 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link'
 import DarkModeSwitch from './DarkModeSwitch';
-
+import About from '@/app/about/page';
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
         <div className="flex">
             <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
-            <MenuItem title="ABOUT" address="https://production-cineverse.vercel.app/about" Icon={BsFillInfoCircleFill}/>
+            <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
         <div className="flex items-center space-x-5">
           <DarkModeSwitch/>
